ea92da1aaa650aa7055b7ee28d8e8d7354bd9ef1
ppss1806 <ppss1806@gmail.com>
2020-06-19 21:48:13 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# seaweedfs-docker
+# Docker
+
+
+## Try it out
+
+```bash
+
+wget https://raw.githubusercontent.com/chrislusf/seaweedfs/master/docker/seaweedfs-compose.yml
+
+docker-compose -f seaweedfs-compose.yml -p seaweedfs up
+
+```
+
+## Try latest tip
+
+```bash
+
+wget https://raw.githubusercontent.com/chrislusf/seaweedfs/master/docker/seaweedfs-dev-compose.yml
+
+docker-compose -f seaweedfs-dev-compose.yml -p seaweedfs up
+
+```
+
+## Local Development
+
+```bash
+cd $GOPATH/src/github.com/chrislusf/seaweedfs/docker
+make
+```
---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+case "$1" in
+
+  'master')
+  	ARGS="-mdir=/data"
+  	exec /usr/bin/weed $@ $ARGS
+	;;
+
+  'volume')
+  	ARGS="-dir=/data"
+  	exec /usr/bin/weed $@ $ARGS
+	;;
+
+  'server')
+  	ARGS="-dir=/data"
+  	exec /usr/bin/weed $@ $ARGS
+  	;;
+
+  'filer')
+  	ARGS=""
+  	exec /usr/bin/weed $@ $ARGS
+	;;
+
+  's3')
+  	ARGS="-domainName=$S3_DOMAIN_NAME -key.file=$S3_KEY_FILE -cert.file=$S3_CERT_FILE"
+  	exec /usr/bin/weed $@ $ARGS
+	;;
+
+  'cronjob')
+	MASTER=${WEED_MASTER-localhost:9333}
+	FIX_REPLICATION_CRON_SCHEDULE=${CRON_SCHEDULE-*/7 * * * * *}
+	echo "$FIX_REPLICATION_CRON_SCHEDULE" 'echo "volume.fix.replication" | weed shell -master='$MASTER > /crontab
+	BALANCING_CRON_SCHEDULE=${CRON_SCHEDULE-25 * * * * *}
+	echo "$BALANCING_CRON_SCHEDULE" 'echo "volume.balance -c ALL -force" | weed shell -master='$MASTER >> /crontab
+	echo "Running Crontab:"
+	cat /crontab
+	exec supercronic /crontab
+	;;
+  *)
+  	exec /usr/bin/weed $@
+	;;
+esac
